66673ebf7b59b8da93855ae8ff06d68f7db717df
Rafael <rafaelmauricioaraujo@gmail.com>
2018-10-26 18:52:17 -0300

    Classes de Testes

---
+++ src/hunger/TesteCliente.java
@@ -0,0 +1,14 @@
+package hunger;
+
+public class TesteCliente {
+	
+	public static void main(String args[]) {
+		
+		Cliente rafael = new Cliente("Rafael");
+		
+		System.out.println(rafael.getNome());
+		System.out.println(rafael.getEndereco());
+		System.out.println(rafael.getTelefone());
+	}
+
+}
