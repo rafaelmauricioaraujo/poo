c1f9535d5807d597f98dfa593ad66021bca4a120
Rafael <rafaelmauricioaraujo@gmail.com>
2018-10-30 16:22:56 -0300

    Atributos e métodos da classe Entregador

--- src/poo/hunger/model/Entregador.java
+++ src/poo/hunger/model/Entregador.java
@@ -2,4 +2,23 @@ package poo.hunger.model;
 
 public class Entregador {
 
+	private String nome;
+	private String placaVeiculo;
+	
+	public Entregador() {
+		
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getNome() {
+		return this.nome;
+	}
+	public void setPlaca(String placaVeiculo) {
+		this.placaVeiculo = placaVeiculo;
+	}
+	public String getPlava() {
+		return this.placaVeiculo;
+	}
 }
