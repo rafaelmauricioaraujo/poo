c317e1ebf49fc35b6fdb0489a4ce21c775708c80
Rafael <rafaelmauricioaraujo@gmail.com>
2018-10-31 16:28:47 -0300

    Classe cheque

--- src/poo/hunger/model/Cheque.java
+++ src/poo/hunger/model/Cheque.java
@@ -1,5 +1,40 @@
 package poo.hunger.model;
 
 public class Cheque {
+		private int numero;
+		private int conta;
+		private int agencia;
+		private int banco;
 		
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+		
+		public int getNumero() {
+			return this.numero;
+		}
+		
+		public void setConta(int conta) {
+			this.conta = conta;
+		}
+		
+		public int getConta() {
+			return this.conta;
+		}
+		
+		public void setAgencia(int agencia) {
+			this.agencia = agencia;
+		}
+		
+		public int getAgencia() {
+			return this.agencia;
+		}
+		
+		public void setBanco(int banco) {
+			this.banco = banco;
+		}
+		
+		public int getBanco() {
+			return this.banco;
+		}
 }
